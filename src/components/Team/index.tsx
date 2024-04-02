59de293c2355a75a04bc9228bdc62bacf7682e08
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-04-02 09:58:28 -0300

    updates

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,18 +1,53 @@
 import { styled } from "styled-components";
 import { Button } from "primereact/button"
-const TeamContainer = styled.section`
-    
+const TeamContainer = styled.div`
+    background-color: #333333;
+    border-radius: 5px;
+    padding: 16px;
+    & h5{
+        font-size: 18px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 0;
+    }
+    & h1{
+        width: 120px;
+        height: 120px;
+        background-color: #464646;
+        border-radius: 50%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 60px;
+        & span{
+            font-size: 16px;
+            display: block;
+        }
+    }
+    & .acoes{
+        display: flex;
+        gap: 16px;
+        margin-top: 16px;
+        & button:first-of-type{
+            flex: 1;
+        }
+    }
 `;
 
 interface TeamProps {
     nome: string;
+    capacidade: number;
+    integrantes: string[];
 }
 
-const Team = ({ nome }: TeamProps) => {
+const Team = ({ nome, capacidade = 0, integrantes = [] }: TeamProps) => {
     return(
         <TeamContainer>
             <h5>{nome} <i className="pi pi-eye"></i></h5>
-            <h1>0/0</h1>
+            <h1>{integrantes.length}<span>/{capacidade}</span></h1>
             <div className="acoes">
                 <Button
                     label="Adicionar"
--- src/components/Teams/index.tsx
+++ src/components/Teams/index.tsx
@@ -2,14 +2,24 @@ import { styled } from "styled-components";
 import Team from "../Team";
 
 const TeamsContainer = styled.section`
-    
+    padding: 26px 100px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 16px;
+    & h2 {
+        width: 100%;
+        margin-bottom: 26px;
+    }
+    & > div{
+        width: calc(20% - 13px);
+    }
 `;
 
 const Teams = () => {
     return(
         <TeamsContainer>
             <h2>Teams</h2>
-            <Team nome={'FS19'}/>
+            <Team nome={'FS19'} capacidade={4} integrantes={['Anderson', 'Ricardy', 'Daniel']}/>
         </TeamsContainer>
     );
 }
